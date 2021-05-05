71723a952c540ba9c4525e9601da1a1c2c0e4cac
Felipe Sales <[soufeliposales@gmail.com]>
2021-05-05 14:47:11 -0300

    Creating BaseScreen

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'screens/base/base_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Store',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: BaseScreen(),
+    );
+  }
+}
---
+++ lib/screens/base/base_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class BaseScreen extends StatelessWidget {
+  final PageController pageController = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      controller: pageController,
+      physics: const NeverScrollableScrollPhysics(),
+      children: [
+        Container(color: Colors.red),
+        Container(color: Colors.yellow),
+        Container(color: Colors.green),
+      ],
+    );
+  }
+}
