545173e463e1a735edb4737392d4388a8ed77176
felipecastrosales <[soufeliposales@gmail.com]>
2021-05-07 15:51:33 -0300

    Perfoming the Login

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../../helpers/validators.dart';
-
 class LoginScreen extends StatelessWidget {
-  final TextEditingController emailController = TextEditingController();
-  final TextEditingController passController = TextEditingController();
-  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,27 +11,21 @@ class LoginScreen extends StatelessWidget {
       body: Center(
         child: Card(
           margin: const EdgeInsets.symmetric(horizontal: 16),
-          child: Form(
-            key: formKey,
+          child: Padding(
+            padding: const EdgeInsets.all(16),
             child: ListView(
-              padding: const EdgeInsets.all(16),
               shrinkWrap: true,
               children: [
                 TextFormField(
-                  controller: emailController,
                   decoration: const InputDecoration(hintText: 'E-mail'),
                   keyboardType: TextInputType.emailAddress,
                   autocorrect: false,
                   validator: (email) {
-                    if (!emailValid(email)) {
-                      return 'Invalid e-mail';
-                    }
                     return null;
                   },
                 ),
                 const SizedBox(height: 16),
                 TextFormField(
-                  controller: passController,
                   decoration: const InputDecoration(hintText: 'Password'),
                   autocorrect: false,
                   obscureText: true,
@@ -51,7 +39,7 @@ class LoginScreen extends StatelessWidget {
                 Align(
                   alignment: Alignment.centerRight,
                   child: TextButton(
-                    onPressed: () {},
+                    onPressed: () {}, 
                     style: TextButton.styleFrom(
                       padding: const EdgeInsets.symmetric(horizontal: 16.0),
                     ),
@@ -62,19 +50,12 @@ class LoginScreen extends StatelessWidget {
                 SizedBox(
                   height: 44,
                   child: ElevatedButton(
-                    onPressed: () {
-                      if (formKey.currentState.validate()) {
-                        debugPrint(emailController.text);
-                      }
-                    },
+                    onPressed: () {},
                     style: ElevatedButton.styleFrom(
                       onPrimary: Theme.of(context).primaryColor,
                       primary: Theme.of(context).primaryColor,
                     ),
-                    child: const Text(
-                      'Enter',
-                      style: TextStyle(fontSize: 18, color: Colors.white),
-                    ),
+                    child: const Text('Enter', style: TextStyle(fontSize: 18)),
                   ),
                 ),
               ],
