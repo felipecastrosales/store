cc5279978404730a61cd2e9785975b1bd35d8958
felipecastrosales <[soufeliposales@gmail.com]>
2021-05-09 21:13:06 -0300

    Loading the User when Opening the App

--- lib/models/user_manager.dart
+++ lib/models/user_manager.dart
@@ -7,23 +7,38 @@ import 'package:store/helpers/firebase_erros.dart';
 import 'user.dart';
 
 class UserManager extends ChangeNotifier {
+  UserManager() {
+    _loadCurrentUser();
+  }
+
   final FirebaseAuth auth = FirebaseAuth.instance;
-  bool loading = false;
+  FirebaseUser user;
+  bool _loading = false;
+  bool get loading => _loading;
 
   Future<void> signIn({User user, Function onSuccess, Function onFail}) async {
-    setLoading(true);
+    loading = true;
     try {
       final AuthResult result = await auth.signInWithEmailAndPassword(
           email: user.email, password: user.password);
+      this.user = result.user;
       onSuccess();
     } on PlatformException catch (e) {
       onFail(getErrorString(e.code));
     }
-    setLoading(false);
+    loading = false;
   }
 
-  void setLoading(bool value) {
-    loading = value;
+  set loading(bool value) {
+    _loading = value;
+    notifyListeners();
+  }
+
+  Future<void> _loadCurrentUser() async {
+    final FirebaseUser currentUser = await auth.currentUser();
+    if (currentUser != null) {
+      user = currentUser;
+    }
     notifyListeners();
   }
 }
